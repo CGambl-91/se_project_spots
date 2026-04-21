39b584f81798a6411a2c83150eaad403093df30d
Chalmer Gambler <c.gambler09@gmail.com>
2026-04-20 18:36:06 -0600

    Add modal open/close functions

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,43 @@ const editButton = document.querySelector(".profile__edit-button");
 const editModal = document.querySelector("#edit-profile-modal");
 const editCloseButton = editModal.querySelector(".modal__close-button");
 
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const profileFormElement = editModal.querySelector(".modal__form");
+const profileNameInput = editModal.querySelector("#profile-name-input");
+const profileDescriptionInput = editModal.querySelector(
+  "#profile-description-input",
+);
+
 const newPostButton = document.querySelector(".profile__post-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
+const newPostFormElement = newPostModal.querySelector(".modal__form");
+const newPostImage = newPostModal.querySelector("#card-image-input");
+const newPostCaption = newPostModal.querySelector("#card-caption-input");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = profileNameInput.value;
+  profileSubtitle.textContent = profileDescriptionInput.value;
+  editModal.classList.remove("modal_is-opened");
+}
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+  console.log(newPostImage.value);
+  console.log(newPostCaption.value);
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+newPostFormElement.addEventListener("submit", handleNewPostSubmit);
 
 editButton.addEventListener("click", function () {
   editModal.classList.add("modal_is-opened");
+  profileNameInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileSubtitle.textContent;
 });
 
 editCloseButton.addEventListener("click", function () {
